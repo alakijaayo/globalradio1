639bf8389a834531d3ceaf716634950296464597
alakijaayo <alakijaayo@gmail.com>
2020-02-26 14:25:41 +0000

    first commit

---
+++ src/test/java/com/example/globalradio/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globalradio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
