82a29982db25a244934c7286f7d7a5c637d6ad56
alakijaayo <alakijaayo@gmail.com>
2020-03-09 17:23:09 +0000

    Added Router Module

--- globalradio-frontend/src/app/app.module.ts
+++ globalradio-frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarListComponent } from './car-list/car-list.component';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { CarListComponent } from './car-list/car-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
